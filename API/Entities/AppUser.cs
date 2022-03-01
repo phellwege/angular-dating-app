cfd49bc1a3433fd822b096922c1b3b8e12a72d5b
Peter Hellwege <peter.hellwege@allata.com>
2022-03-01 10:09:27 -0700

    4th commit

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,4 @@
-namespace Api.Entities
+namespace API.Entities
 {
     public class AppUser
     {
--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
+using API.Data;
 
 namespace API
 {
