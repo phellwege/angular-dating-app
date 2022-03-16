52cdd5f64ec2d32d7cd8c79ce5c1cee86c0d8aac
Peter Hellwege <peter.hellwege@allata.com>
2022-03-16 08:16:21 -0600

    added user controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API.Controllers;
+    [ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext context;
+    public UsersController(DataContext context)
+    {
+        this.context = context;
+    }
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        return context.Users.ToList();
+    }
+    [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        return context.Users.Find();
+    }
+}
